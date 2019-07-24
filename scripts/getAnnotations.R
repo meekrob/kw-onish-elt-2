7ea26bd6ed23abdc41bcf6e79cad0c7b2ea00e77
David King <davidcking.inbox@gmail.com>
2019-07-23 19:13:20 -0600

    script to read peaks, annotate automatically

--- scripts/getAnnotations.R
+++ scripts/getAnnotations.R
@@ -18,3 +18,8 @@ listDatasets(mart = ensembl) # Use number 29: celegans_gene_ensembl
 mart = useMart(biomart = "ENSEMBL_MART_ENSEMBL", host="grch37.ensembl.org", path="/biomart/martservice", dataset = "celegans_gene_ensembl")
 # Get TSS
 ce_TSS = getAnnotation(mart, featureType = "TSS")
+
+source('scripts/tenthousandPeaksAsGRanges.R') # defines "peaks"
+annotated_peaks = annotatePeakInBatch(peaks,mart, featureType = "TSS", AnnotationData = ce_TSS)
+wbid=unlist(strsplit(names(annotated_peaks), "[.]"))[seq(2,2*length(annotated_peaks),by=2)]
+annotated_peaks$WBID = wbid
