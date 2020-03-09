fbe2dfda2d74a94cce1adcedbb7bb37b03f3110a
David King <davidcking.inbox@gmail.com>
2020-03-09 02:58:22 -0600

    bug fixes. usage notes

--- kweights.R
+++ kweights.R
@@ -1,3 +1,8 @@
+# unnormed gotten by cbinding 3 random vectors
+# normed: mean subtracted, sd divided
+# normedk = data.frame(normed,k=k$cluster)
+# normed_weights = apply(normedk, 1, row_func)
+# plot_ly(rbind(unnormed,rep(1,3),rep(-1,3),rep(0,3),centers), x=~x,y=~y,z=~z,type="scatter3d", mode='markers',color=as.factor(c(k$cluster,0,0,0,4,5,6)), colors=c('#666666',   '#0000FF','#00FF00','#FF0000','#000088','#008800','#880000'), size= 5*c(normed_weights,rep(1,6)))
 
 row_func = function(r) {
   # cluster matrix C must exist outside scope of this function
@@ -5,11 +10,11 @@ row_func = function(r) {
   n = length(r);
   x = r[1:(n-1)];
   k = r[n];
-  return(weights_f = function(c) {
-    weight_f(c,x,C[k,]);
-    apply(C,2,weights_f)->weights;
-    return(1/sum(weights));
-  });
+  weights_f = function(c) {
+    return(weight_f(c,x,C[k,]));
+  }
+  apply(C,1,weights_f)->weights;
+  return(1/sum(weights));
 }
 
 weight_f = function(c,v,ck,m=2) {
