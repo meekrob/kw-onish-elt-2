698bb37c24eb2799c2169d650d6fe545c32fbc09
David King <davidcking.inbox@gmail.com>
2020-03-10 14:26:42 -0600

    corrected flatten_std,added polar/xy conversions, and ring plot

--- rotation_matrices.R
+++ rotation_matrices.R
@@ -21,5 +21,45 @@ Rz = function(theta) {
 }
 
 flatten_std = function(p) {
-  return(p %*% Rz(-pi/2) %*% Ry(pi/2));
+  return(p %*% Rx(pi/2) %*% Rz(pi/6));
+}
+
+polar_to_xy = function(r, theta) {
+  x = r * cos(theta);
+  y = r * sin(theta);
+  return(cbind(x,y));
+}
+
+xy_to_polar = function(x,y) {
+  r = sqrt(x^2 + y^2);
+  theta = atan(y/x);
+  return(cbind(r,theta));
+}
+
+updateXY = function(df) {
+
+    xy = polar_to_xy(df$r,df$theta)
+    df$x = xy[,'x']
+    df$x = xy[,'y']
+    return(df);
+  
+}
+
+updatePolar = function(df) {
+  polar = xy_to_polar(df$x,df$y)
+  df$r = polar[,'r'];
+  df$theta = polar[,'theta'];
+  return(df);
+}
+
+getArcSeries = function(r, startTheta, endTheta, n=10) {
+  angles = seq(startTheta,endTheta,length.out=n);
+  xy = polar_to_xy( rep(r,n), angles)
+  return(xy)
+}
+
+ring = function(offset, thickness, startTheta, endTheta, n=10) {
+  innerPoints = getArcSeries(offset, startTheta,endTheta,n);
+  outerPoints = getArcSeries(offset+thickness, endTheta,startTheta,n); # in reverse order
+  return(rbind(innerPoints,outerPoints));
 }
