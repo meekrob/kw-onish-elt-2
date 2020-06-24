c09172d977cdffbf924a57ff811fb6e72fc1ade7
David King <davidcking.inbox@gmail.com>
2020-06-23 22:19:22 -0600

    updates to summit scripts

--- scripts/summit_scripts/extract_fa.py
+++ scripts/summit_scripts/extract_fa.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 import sys,os,textwrap
 
-SEQDIR='/projects/dcking@colostate.edu/support_data/ce11'
+#SEQDIR='/projects/dcking@colostate.edu/support_data/ce11'
+SEQDIR='/projects/dcking@colostate.edu/support_data/ce11/Nmasked'
+
 SEQDIR_CONTENTS = os.listdir(SEQDIR)
 seqs = {}
 infile = sys.argv[1]
+lflank = 0
+rflank = 0
+
+if len(sys.argv) > 2:
+    lflank = int(sys.argv[2])
+    rflank = lflank
+    if len(sys.argv) > 3:
+        rflank = int(sys.argv[3])
+
 for line in open(infile):
     fields = line.strip().split()
-    chrom, start, end = fields[0], int(fields[1]), int(fields[2])
+    chrom, start, end = fields[0], int(fields[1]) - lflank, int(fields[2]) + rflank
 
+    # OPEN SEQUENCE FILE IF NOT ALREADY OPEN
     if chrom not in seqs :
         if chrom + ".fa" not in SEQDIR_CONTENTS:
             print("warning: no seqfile for chromosome", chrom, file = sys.stderr)
@@ -19,9 +31,18 @@ for line in open(infile):
             header = fh.readline().strip()
             seq = fh.read().replace("\n", "")
             seqs[chrom] = { 'header': header, 'seq': seq }
+            fh.close()
             
 
-    print("> %s:%d-%d" % (chrom,start,end))
-    print("\n".join(textwrap.wrap( seqs[chrom]['seq'][start:end], 150)))
+    # EXTRACT AND PRINT SEQUENCE
+    # prevent indexing from tripping on out of range values
+    start = max(start, 0)
+    end = min(end, len(seqs[chrom]['seq']))
+    flankText = ''
+    if lflank > 0 or rflank > 0:
+        flankText = " (-%d,+%d)" % (lflank,rflank)
+    print(">%s:%d-%d%s" % (chrom,start,end,flankText))
+    LINEWIDTH = 150
+    print("\n".join(textwrap.wrap( seqs[chrom]['seq'][start:end], LINEWIDTH)))
     
 
