9d57057936088440f5dfc2b255b037275e5a0429
David King <davidcking.inbox@gmail.com>
2020-07-06 15:55:52 -0600

    Added a version to read in the modENCODE data downloaded from the genome browser.

--- scripts/makeGRangesFromNarrowPeak.R
+++ scripts/makeGRangesFromNarrowPeak.R
@@ -5,6 +5,27 @@ if (!requireNamespace("GenomicRanges", quietly = TRUE)) {
     BiocManager::install("GenomicRanges")
 }
 library(GenomicRanges, warn.conflicts=F);
+makeGRangesFromEncodeNarrowPeak <- function(filename) {
+    narrowPeak_df = read.table(filename, 
+           col.names=c('chrom', 
+                       'comprehensive_start', 
+                       'comprehensive_end',   
+                       'name', 'score', 'strand',  
+                       'signal',
+                       'minus_log_p',     
+                       'minus_log_q','offset'))
+    narrowPeak_df$score = NULL; # always 0
+    narrowPeak_df$strand = NULL; # always '.'
+    narrowPeak_df$name = NULL; # always '.'
+    narrowPeak_df$minus_log_p = NULL; # always -1
+    
+    narrowPeak_gr = makeGRangesFromDataFrame(narrowPeak_df, starts.in.df.are.0based=T, 
+                                             keep.extra.columns=T, 
+                                             start.field="comprehensive_start",
+                                             end.field="comprehensive_end",
+                                             ignore.strand=T);
+    return(narrowPeak_gr);
+}
 makeGRangesFromNarrowPeak <- function(filename) {
     narrowPeak_df = read.table(filename, 
         col.names=c('chrom', 
