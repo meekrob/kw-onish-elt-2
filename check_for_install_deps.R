9a07acdb1ad7843a5bbae83bd4a3a7c6fa9fa8cb
David C. King <davidcking.inbox@gmail.com>
2020-07-08 15:23:18 -0600

    Made a script to centralize dependency installation.

---
+++ check_for_install_deps.R
@@ -0,0 +1,28 @@
+# standard packages
+pkgs = c(
+  "BiocManager",
+  "reshape2",
+  "stringr",
+  "pheatmap",
+  "ggplot2",
+  "ggExtra",
+  "RColorBrewer",
+  "VennDiagram",
+  "dplyr",
+  "knitr"
+)
+need.pkgs = pkgs[! (pkgs %in% installed.packages()[,"Package"])]
+if (length(need.pkgs)) {install.packages(need.pkgs, update=F) }
+
+# Bioconductor packages
+pkgs = c("biomaRt",
+         "GenomicRanges", 
+         "ChIPpeakAnno",
+         "topGO", 
+         "UpSetR")
+
+need.pkgs = pkgs[! (pkgs %in% installed.packages()[,"Package"])]
+if (length(need.pkgs)) {
+  library(BiocManager)
+  BiocManager::install(need.pkgs,update=F)
+}
