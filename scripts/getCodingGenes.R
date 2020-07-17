2eaa81d6940995c87f38bc4ea1494b15f224b7bf
David C. King <davidcking.inbox@gmail.com>
2020-07-16 19:18:23 -0600

    resolved gene mapping. See previous commits for methods.

--- scripts/getCodingGenes.R
+++ scripts/getCodingGenes.R
@@ -141,54 +141,12 @@ getCodingGenes = function(peaks, within_genes_kb = 5){
   # The output is X00001.WBID. The peak ID (in the name column) is more useful.
   names(ap) <- ap$name
   # these fields got converted to character during bigbed output
-  # TODO- just do that conversion during the export
-  ap$k4weights = as.numeric(ap$k4weights)
-  ap$k11weights = as.numeric(ap$k11weights)
-  ap$LE_nonNormed = as.numeric(ap$LE_nonNormed)
-  ap$L1_nonNormed = as.numeric(ap$L1_nonNormed)
-  ap$L3_nonNormed = as.numeric(ap$L3_nonNormed)
-  ap$L3_std = as.numeric(ap$L3_std)
-  ap$L1_std = as.numeric(ap$L1_std)
-  ap$LE_std = as.numeric(ap$LE_std)
-  ap$peak = as.integer(ap$peak)
   
-  # unmapped +/- 5Kb
-  nappy = ap[is.na(ap$feature)]
-  # mapped
-  ap = ap[!is.na(ap$feature)]
-  # reduce list down to the minumum absolute value of distancetoFeature
-  ap.ordered = ap[ order(ap$name, abs(ap$distancetoFeature) )]
-  ap.unique = ap[ match(unique(ap.ordered$name), ap.ordered$name)]
-  ap.rejoined = c(ap.unique, nappy)
-  ap.rejoined = ap.rejoined[ order(ap.rejoined$name)]
   
-  ap = ap.rejoined
-  insideFeatureLabels = as.character(ap$insideFeature)
-  insideFeatureLabels[is.na(ap$feature)] <- sprintf("unmapped ± %dKb",within_genes_kb) 
-  insideFeatureLabels[ap$shortestDistance > within_genes_kb * 1000] <- sprintf("unmapped ± %dKb",5) 
-  ap$insideFeature <- as.factor(insideFeatureLabels)
-  # does not differ between clusters, with overlapStart and upstream accounting for 60-70% of the annotation types (followed by 'inside', then 'downstream')
-  round(table(ap$k4cluster, ap$insideFeature)/apply(table(ap$k4cluster, ap$insideFeature), 1, sum),3)*100
-  
-  ap$gene_name = all_CDS_genes[ ap$feature]$name 
-  stacked_nr$gene_name = all_CDS_genes[ stacked_nr$feature]$name 
-  
-  # ap broken down by kclust number
-  ap_0 = ap[ap$k4cluster ==0]
-  ap_1 = ap[ap$k4cluster ==1]
-  ap_2 = ap[ap$k4cluster ==2]
-  ap_3 = ap[ap$k4cluster ==3]
-  ap_4 = ap[ap$k4cluster ==4]
   saveRDS(ap, "annotatedPeaks.rds")
   return(
     list(ap=ap, 
          stacked_nr=stacked_nr,
-         bycluster=list(
-           ap_0=ap_0,
-           ap_1=ap_1,
-           ap_2=ap_2,
-           ap_3=ap_3,
-           ap_4=ap_4),
          all_CDS_genes=all_CDS_genes
          )
     )
