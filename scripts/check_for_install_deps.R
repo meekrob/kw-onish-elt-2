608c9ea8c28fecc0748da6ba70aa83d3eee81468
David <dcking@colostate.edu>
2020-11-07 21:09:17 -0700

    Added scripts to scripts/

---
+++ scripts/check_for_install_deps.R
@@ -0,0 +1,29 @@
+# standard packages
+pkgs = c(
+  "BiocManager",
+  "reshape2",
+  "stringr",
+  "pheatmap",
+  "ggplot2",
+  "ggExtra",
+  "RColorBrewer",
+  "VennDiagram",
+  "dplyr",
+  "knitr"
+)
+need.pkgs = pkgs[! (pkgs %in% installed.packages()[,"Package"])]
+if (length(need.pkgs)) {install.packages(need.pkgs, update=F) }
+
+# Bioconductor packages
+pkgs = c("biomaRt",
+         "GenomicRanges", 
+         "ChIPpeakAnno",
+         "topGO", 
+         "plyranges", # tidy the GenomicRanges objects
+         "UpSetR")
+
+need.pkgs = pkgs[! (pkgs %in% installed.packages()[,"Package"])]
+if (length(need.pkgs)) {
+  library(BiocManager)
+  BiocManager::install(need.pkgs,update=F)
+}
